edbbc7acf01674181049e78282a81ae83166c22b
mcgarryplace-michael <software.engineer@mcgarryplace.com>
2017-04-15 11:21:50 -0500

    Replaced tabs with spaces

--- devicetypes/mcgarryplace-michael/stateless-virtual-switch.src/stateless-virtual-switch.groovy
+++ devicetypes/mcgarryplace-michael/stateless-virtual-switch.src/stateless-virtual-switch.groovy
@@ -17,54 +17,54 @@
  *  Date: 2017-04-15
  */
 metadata {
-	definition (name: "Stateless Virtual Switch", namespace: "mcgarryplace-michael", author: "Michael McGarry") {
-		capability "Actuator"
-		capability "Switch"
-		capability "Sensor"
+    definition (name: "Stateless Virtual Switch", namespace: "mcgarryplace-michael", author: "Michael McGarry") {
+        capability "Actuator"
+        capability "Switch"
+        capability "Sensor"
         
-		attribute "info", "string"
-	}
+        attribute "info", "string"
+    }
 
-	// simulator metadata
-	simulator {
-	}
+    // simulator metadata
+    simulator {
+    }
 
-	// UI tile definitions
-	tiles {
-		standardTile("button", "device.button", width: 1, height: 1) {
-			state "default", label: "", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
-		}
+    // UI tile definitions
+    tiles {
+        standardTile("button", "device.button", width: 1, height: 1) {
+            state "default", label: "", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
+        }
     
- 		standardTile("pushOn", "device.button", width: 1, height: 1, decoration: "flat") {
-			state "default", label: "On", backgroundColor: "#ffffff", action: "on"
-		} 
+         standardTile("pushOn", "device.button", width: 1, height: 1, decoration: "flat") {
+            state "default", label: "On", backgroundColor: "#ffffff", action: "on"
+        } 
         
- 		standardTile("pushOff", "device.button", width: 1, height: 1, decoration: "flat") {
-			state "default", label: "Off", backgroundColor: "#ffffff", action: "off"
-		}   
+         standardTile("pushOff", "device.button", width: 1, height: 1, decoration: "flat") {
+            state "default", label: "Off", backgroundColor: "#ffffff", action: "off"
+        }   
         
         valueTile("information", "device.info", inactiveLabel: false, decoration: "flat", width: 6, height:2) {
             state "info", label:'${currentValue}'
-		}
+        }
         
-		main "button"
-		details (["pushOn", "pushOff", "information"])
-	}
+        main "button"
+        details (["pushOn", "pushOff", "information"])
+    }
 }
 
 def installed() {
-	sendEvent(name: "info", value: "Greetings and Salutations.") 
+    sendEvent(name: "info", value: "Greetings and Salutations.") 
 }
 
 def parse(String description) {
 }
 
 def on() {
-	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
-	sendEvent(name: "info", value: "Sending On Command", isStateChange: true, displayed: true) 
+    sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+    sendEvent(name: "info", value: "Sending On Command", isStateChange: true, displayed: true) 
 }
 
 def off() {
-	sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
-	sendEvent(name: "info", value: "Sending Off Command", isStateChange: true, displayed: true) 
+    sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
+    sendEvent(name: "info", value: "Sending Off Command", isStateChange: true, displayed: true) 
 }
